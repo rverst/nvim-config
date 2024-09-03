b5b4eeb90352b44d82392b9896ac9bade8b6c07c
Robert Verst <robert.verst@fiege.com>
2024-09-03 13:58:11 +0200

    Update which-key config

--- lua/rv/plugins/which-key.lua
+++ lua/rv/plugins/which-key.lua
@@ -5,24 +5,26 @@
 return {
   'folke/which-key.nvim',
   enabled = true,
-  event = 'VimEnter',
-  config = function()
-    require('which-key').setup({
-      show_help = false,
-      show_keys = false,
-    })
-
-    -- Document existing key chains
-    require('which-key').register({
-      ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-      ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-      ['<leader>f'] = { name = '[F]ind', _ = 'which_key_ignore' },
-      ['<leader>g'] = { name = '[G]enerate', _ = 'which_key_ignore' },
-      ['<leader>h'] = { name = '[H]arpoon', _ = 'which_key_ignore' },
-      ['<leader>o'] = { name = '[O]pen', _ = 'which_key_ignore' },
-      ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-      ['<leader>u'] = { name = '[U]tils', _ = 'which_key_ignore' },
-      ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-    })
-  end,
+  event = 'VeryLazy',
+  opts = {},
+  keys = {
+    { '<leader>c', group = '[C]ode' },
+    { '<leader>c_', hidden = true },
+    { '<leader>d', group = '[D]ocument' },
+    { '<leader>d_', hidden = true },
+    { '<leader>f', group = '[F]ind' },
+    { '<leader>f_', hidden = true },
+    { '<leader>g', group = '[G]enerate' },
+    { '<leader>g_', hidden = true },
+    { '<leader>h', group = '[H]arpoon' },
+    { '<leader>h_', hidden = true },
+    { '<leader>o', group = '[O]pen' },
+    { '<leader>o_', hidden = true },
+    { '<leader>r', group = '[R]ename' },
+    { '<leader>r_', hidden = true },
+    { '<leader>u', group = '[U]tils' },
+    { '<leader>u_', hidden = true },
+    { '<leader>w', group = '[W]orkspace' },
+    { '<leader>w_', hidden = true },
+  },
 }
